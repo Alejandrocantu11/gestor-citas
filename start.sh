e04a9d6d4e33bd297a7e36d0e781d0e454bec60e
Alejandro Cantu <alex.cantu04@gmail.com>
2025-03-26 18:57:54 -0600

    Add start.sh for Railway

--- start.sh
+++ start.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-composer install
+
+composer install --no-interaction --prefer-dist --optimize-autoloader
 php artisan key:generate
 php artisan migrate --force
 php artisan serve --host=0.0.0.0 --port=$PORT
+
