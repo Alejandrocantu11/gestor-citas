1748ad91792b3b8f7739c678dd17c104fd3862d1
Alejandro Cantu <alex.cantu04@gmail.com>
2025-03-26 19:41:56 -0600

    Add config:clear to start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 composer install --no-interaction --prefer-dist --optimize-autoloader
+php artisan config:clear
 php artisan key:generate
 php artisan migrate --force
 php artisan serve --host=0.0.0.0 --port=$PORT
+
 
